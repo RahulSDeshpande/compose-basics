bd209607e48c542bbedc1e32f1e7c3ce7c958ead
Rahul Deshpande <rahul.deshpande92@gmail.com>
2023-12-26 15:34:45 +0400

    Advance designs - Scaffold + TopAppBar [WIP]

--- app/src/main/java/com/mojolabs/composedemo1/feature/advancedesigns/AdvanceDesignsActivity.kt
+++ app/src/main/java/com/mojolabs/composedemo1/feature/advancedesigns/AdvanceDesignsActivity.kt
@@ -15,11 +15,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,9 +52,26 @@ class AdvanceDesignsActivity : ComponentActivity() {
 
 @Composable
 private fun AdvanceDesignsMainScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        ProfileCard()
+    Scaffold(topBar = { AppBar() }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            ProfileCard()
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = { Text(text = "TopAppBar") },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "Top app bar nav icon",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    )
 }
 
 @Composable
