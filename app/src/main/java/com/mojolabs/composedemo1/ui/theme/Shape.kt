e877c71a61a5275216503e2f4cca7d47ae09b417
Rahul Deshpande <rahul.deshpande92@gmail.com>
2023-12-27 11:41:42 +0400

    Advance designs - Shapes

---
+++ app/src/main/java/com/mojolabs/composedemo1/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.mojolabs.composedemo1.ui.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes =
+    Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = CutCornerShape(topEnd = 4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
